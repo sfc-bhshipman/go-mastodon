a47e0920501c02851bdeafe940a655fe4c49284d
Rasmus Lindroth <rasmus@lindroth.xyz>
2022-11-23 19:00:12 +0100

    add tags and fix test for lists

---
+++ tags.go
@@ -0,0 +1,46 @@
+package mastodon
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+// Tag hold information for tag.
+type Tag struct {
+	Name      string      `json:"name"`
+	URL       string      `json:"url"`
+	History   []History   `json:"history"`
+	Following interface{} `json:"following"`
+}
+
+// TagInfo gets statistics and information about a tag
+func (c *Client) TagInfo(ctx context.Context, tag string) (*Tag, error) {
+	var hashtag Tag
+	err := c.doAPI(ctx, http.MethodGet, fmt.Sprintf("/api/v1/tags/%s", url.PathEscape(string(tag))), nil, &hashtag, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &hashtag, nil
+}
+
+// TagFollow lets you follow a hashtag
+func (c *Client) TagFollow(ctx context.Context, tag string) (*Tag, error) {
+	var hashtag Tag
+	err := c.doAPI(ctx, http.MethodPost, fmt.Sprintf("/api/v1/tags/%s/follow", url.PathEscape(string(tag))), nil, &hashtag, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &hashtag, nil
+}
+
+// TagUnfollow lets you unfollow a hashtag
+func (c *Client) TagUnfollow(ctx context.Context, tag string) (*Tag, error) {
+	var hashtag Tag
+	err := c.doAPI(ctx, http.MethodPost, fmt.Sprintf("/api/v1/tags/%s/unfollow", url.PathEscape(string(tag))), nil, &hashtag, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &hashtag, nil
+}
